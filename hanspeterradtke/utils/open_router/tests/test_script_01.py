fb9aff17ba3d83ca75ec71544472a5c73bfe6bcc
ich? <=>
2025-09-22 18:50:31 +0200

    removed old package

--- hanspeterradtke/utils/open_router/tests/test_script_01.py
+++
@@ -1,6 +0,0 @@
-def main():
-    # Intentional error: calling a non-existent function
-    cupoff("this will cause an error")
-
-if __name__ == "__main__":
-    main()
--- hanspeterradtke/utils/open_router/tests/test_script_02.py
+++
@@ -1,7 +0,0 @@
-def calculate():
-    # Another intentional error: undefined variable
-    result = foo + 1
-    return result
-
-if __name__ == "__main__":
-    print(calculate())
