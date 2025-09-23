dd8a2476756ca745de4b46f4ea8a81f28c54441d
ich? <=>
2025-09-23 19:24:30 +0200

    Expose accessibility functions via ui_interface module __init__

--- hpr/utils/ui_interface/__init__.py
+++ hpr/utils/ui_interface/__init__.py
@@ -1,14 +1,13 @@
-"""
-UI Interface Module for X11 (LXDE)
+from .accessibility import (
+  list_interactable_elements,
+  type_into_document,
+  read_document_text,
+  click_element
+)
 
-Provides functions to interact with top-level windows:
-- List windows
-- Focus a window
-- Close a window
-- Launch applications
-- Send keystrokes/mouse events
-
-Depends on: wmctrl, xdotool
-"""
-
-from .main import list_windows, focus_window, close_window, launch_app, send_keys
+__all__ = [
+  "list_interactable_elements",
+  "type_into_document",
+  "read_document_text",
+  "click_element"
+]
